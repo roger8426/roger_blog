56760c12a67e8d13f7853998fe92118e5308f2c8
Roger.Lo <roger.lo@skylinetw.com>
2025-05-07 15:20:50 +0800

    feat: first commit

---
+++ .prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  trailingComma: "es5",
+  arrowParens: "avoid",
+  endOfLine: "auto",
+};
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+import { defineNuxtConfig } from "nuxt/config";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint", "@nuxt/content"],
+  css: ["~/assets/tailwind.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
