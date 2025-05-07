f22d591c280cc2d768ef9071a140f42163d73378
Roger.Lo <roger.lo@skylinetw.com>
2025-05-07 17:23:40 +0800

    chore: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
+import prettierPlugin from "eslint-plugin-prettier";
 
-export default withNuxt();
-// Your custom configs here
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default await withNuxt({
+  // 加入自定義配置
+  plugins: {
+    prettier: prettierPlugin,
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "vue/multi-word-component-names": "off",
+  },
+  languageOptions: {
+    globals: {
+      defineNuxtConfig: "readonly",
+      defineNuxtPlugin: "readonly",
+      useRoute: "readonly",
+      useRouter: "readonly",
+      useState: "readonly",
+    },
+  },
+});
